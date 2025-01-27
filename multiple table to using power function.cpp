b1c0b89f561951dc1e1e5492925913d342407887
Soumita125 <soumitap099@gmail.com>
2025-01-27 16:17:26 +0530

    Add files via upload

---
+++ multiple table to using power function.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("enter a number:5");
+	scanf("%d", &n);
+	printf("multiplication table of %d:\n", n);
+	printf("\n");
+	printf("%d*%d=%d\n",n,i,n*i);
+	return 0;
+}
+
