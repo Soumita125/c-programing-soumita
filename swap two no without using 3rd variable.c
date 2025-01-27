85095414bdc7c39b4985f9d7a335fbf5625ae7db
Soumita125 <soumitap099@gmail.com>
2025-01-27 16:20:29 +0530

    Add files via upload

---
+++ swap two no without using 3rd variable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main() 
+{
+    float p,r,t,amt,ci;
+    scanf("%f%f%f",&p,&r,&t);
+    amt=p*pow((1+r/100),t);
+    ci=amt-p;
+    printf("%f",ci);
+    return 0;
+}
+
