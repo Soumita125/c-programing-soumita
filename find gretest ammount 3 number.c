09cb627a44827abc91a8a8ac706d78b6a46a9f61
Soumita125 <soumitap099@gmail.com>
2025-02-10 16:35:16 +0530

    Add files via upload

---
+++ find gretest ammount 3 number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if ("devided by 400");
+	{
+		printf("the year is leap year");
+	}
+	else if("devided by 100");
+	{
+		printf ("the year is not leap year");
+	}
+	else if("devided by 4");
+	{
+		printf("the year is leap year");
+	}
+	else if("devided by 3");
+	{
+		printf("the year is not leap year");
+	}
+	return 0;
+}
