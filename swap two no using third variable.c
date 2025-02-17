f29eb98ca433a420447edc0daba3b8d934369753
Soumita125 <soumitap099@gmail.com>
2025-02-17 20:05:20 +0530

    Update swap two no using third variable.c

--- swap two no using third variable.c
+++ swap two no using third variable.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main() 
 {
-    int a,b;
+    int a,b,c;
     scanf("%d", &a);
     scanf("%d",&b);
 	printf("original value Before swapping:%d\n%d\n", a, b);
-    a=a+b;
-    b=a-b;
-    a=a-b;
-    printf("After swapping: a = %d, b = %d\n", a, b);
+    c=0;
+	c=a;
+	a=b;
+	b=c;
+    printf("After swapping= "%d\n%d/n"), a, b);
 	return 0;
 }
 
